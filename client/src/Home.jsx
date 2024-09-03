0a9a301a018526f214827ccfb943dfeb8ec1ac83
Rupendra <136944198+RupendraCHA@users.noreply.github.com>
2024-09-03 23:40:31 +0530

    Image Link

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -5,8 +5,11 @@ function Home() {
     return (
         <div className='bg-dark d-flex flex-column justify-content-center align-items-center vh-100'>
             <div>
-                <img src='https://res.cloudinary.com/dvxkeeeqs/image/upload/v1724307774/analyticsinsight_import_wp-content_uploads_2021_09_Anji-G-Reddy_ut6aui.jpg'
-                    className='w-100 mt-3 mb-3 rounded-2' />
+                <a href="https://app.powerbi.com/links/JTnfzld7WT?ctid=d77ec4e8-c88b-431b-8d7f-77755a4bbefc&pbi_source=linkShare">
+                    <img src='https://res.cloudinary.com/dvxkeeeqs/image/upload/v1724307774/analyticsinsight_import_wp-content_uploads_2021_09_Anji-G-Reddy_ut6aui.jpg'
+                        className='w-100 mt-3 mb-3 rounded-2' />
+                </a>
+
             </div>
             <h1 className='text-success '>
                 Welcome to the Visionsoft Aspac Pvt Ltd., Have a wounderful day!
