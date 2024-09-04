b96184154659293e0b9a651e675b8c08c2d0b213
Rupendra <136944198+RupendraCHA@users.noreply.github.com>
2024-09-04 10:41:44 +0530

    Changing Image link to Text Link

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -5,9 +5,10 @@ function Home() {
     return (
         <div className='bg-dark d-flex flex-column justify-content-center align-items-center vh-100'>
             <div>
-                <a href="https://app.powerbi.com/links/JTnfzld7WT?ctid=d77ec4e8-c88b-431b-8d7f-77755a4bbefc&pbi_source=linkShare">
-                    <img src='https://res.cloudinary.com/dvxkeeeqs/image/upload/v1724307774/analyticsinsight_import_wp-content_uploads_2021_09_Anji-G-Reddy_ut6aui.jpg'
-                        className='w-100 mt-3 mb-3 rounded-2' />
+                <a target='_blank' style={{ textDecoration: "none" }} href="https://app.powerbi.com/links/JTnfzld7WT?ctid=d77ec4e8-c88b-431b-8d7f-77755a4bbefc&pbi_source=linkShare">
+                    {/* <img src='https://res.cloudinary.com/dvxkeeeqs/image/upload/v1724307774/analyticsinsight_import_wp-content_uploads_2021_09_Anji-G-Reddy_ut6aui.jpg'
+                        className='w-100 mt-3 mb-3 rounded-2' /> */}
+                    <h1 style={{ padding: "20px", fontFamily: "Roboto", borderRadius: "12px", backgroundColor: "white", border: "3px solid #ff0000" }}>Hanelytics</h1>
                 </a>
 
             </div>
@@ -23,7 +24,7 @@ function Home() {
                     </button>
                 </Link>
             </div>
-        </div>
+        </div >
     )
 }
 
